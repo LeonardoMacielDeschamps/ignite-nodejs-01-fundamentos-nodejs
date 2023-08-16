dfbc3c005a04f6d7963a329b29df4da26c79a168
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-08-15 21:10:15 -0300

    Streams no Node.js - Criando stream de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,20 @@
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      }
+      else {
+        this.push(Buffer.from(String(i)));
+      }
+    }, 1000);
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
